554c1e295b9e29e238b6ed3c0fb1981b06a6321d
miueon <cruxbell@gmail.com>
2022-06-15 13:54:58 +0800

    task3

--- src/philphix.c
+++ src/philphix.c
@@ -35,7 +35,7 @@ HashTable *dictionary;
 
 /*
  * The MAIN routine.  You can safely print debugging information
- * to standard error (stderr) as shown and it will be ignored in 
+ * to standard error (stderr) as shown and it will be ignored in
  * the grading process.
  */
 #ifndef _PHILPHIX_UNITTEST
@@ -65,14 +65,88 @@ int main(int argc, char **argv) {
 }
 #endif /* _PHILPHIX_UNITTEST */
 
+int allowedKeyChar(char s) {
+  if (s >= 'A' && s <= 'Z') {
+    return 1;
+  } else if (s >= 'a' && s <= 'z') {
+    return 1;
+  } else if (s >= '0' && s <= '9') {
+    return 1;
+  }
+  return 0;
+}
+
+char *stripFirst(char *s) {
+  char *base = s;
+  while (base[0] == '\0' || base[0] == ' ' || base[0] == '\t') {
+    base += 1;
+  }
+  return base;
+}
+
+int isEmpty(const char *s) {
+  while (*s) {
+    if (!isspace(*s)) {
+      return 0;
+    }
+    s++;
+  }
+  return 1;
+}
+
+int getKeySize(char *s) {
+  int size = 0;
+  while (allowedKeyChar(s[size])) {
+    size++;
+  }
+  return size;
+}
+
+char *getWord(char *base, int size) {
+  char *word = malloc(size * sizeof(char));
+  strncpy(word, base, size);
+  return word;
+}
+
+int getDataSize(char *s) {
+  int size = 0;
+
+  while (s[size] != ' ' && s[size] != '\t' && s[size] != '\0' &&
+         s[size] != '\n') {
+    size++;
+  }
+  return size;
+}
 /* Task 3 */
 void readDictionary(char *dictName) {
   // -- TODO --
-  fprintf(stderr, "You need to implement readDictionary\n");
+  // fprintf(stderr, "You need to implement readDictionary\n");
+  FILE *dictFile = fopen(dictName, "r");
+  if (dictFile == NULL) {
+    fprintf(stderr, "file name=%s, not exits", dictName);
+    exit(61);
+  }
+  char *line = NULL;
+  size_t len = 0;
+  size_t read;
+
+  while ((read = getline(&line, &len, dictFile)) != -1) {
+    if (isEmpty(line)) {
+      continue;
+    }
+    char *base = stripFirst(line);
+    int keySize = getKeySize(base);
+    char *key = getWord(base, keySize);
+    base = stripFirst(base + keySize);
+    int dataSize = getDataSize(base);
+    char *data = getWord(base, dataSize);
+    insertData(dictionary, key, data);
+  }
 }
 
 /* Task 4 */
 void processInput() {
   // -- TODO --
-  fprintf(stderr, "You need to implement processInput\n");
+  // fprintf(stderr, "You need to implement processInput\n");
+  
 }
