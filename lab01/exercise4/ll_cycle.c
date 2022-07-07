2affa1b51fba50af9546bcb6fa87fef7ca7096ec
miueon <cruxbell@gmail.com>
2022-07-07 12:27:24 +0800

    lab1 ex4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,20 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+  node *fast = head;
+  node *slow = head;
+  while (fast != NULL) {
+    if (fast->next != NULL) {
+      fast = fast->next->next;
+      slow = slow->next;
+    } else {
+      return 0;
+    }
+    if (slow == fast)
+    {
+    return 1;
+    }
+  }
+  return 0;
 }
