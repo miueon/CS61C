aa0978a81f9776bda4ef71befa8c42b987963780
miueon <cruxbell@gmail.com>
2022-08-22 12:58:54 +0800

    1

--- src/matmul.s
+++ src/matmul.s
@@ -41,29 +41,40 @@ widthA_heightB_uneuqal:
     # Prologue
 prologue:
     addi sp, sp, 
-    sw s0, 
-
+    sw s0, 0(sp) // outer_loop_counter
+    sw s1, 4(sp) // inner_loop_counter
+    sw s2, 8(sp) // m0 ptr 
+    sw s3, 12(sp) // m0 height 
+    sw s4, 16(sp) // m0 width 
+    sw s5, 20(sp) // m1 ptr
+    sw s6, 24(sp) // m1 height
+    sw s7, 28(sp) // m1 width
+    sw s8, 32(sp) // dest ptr
+    mv s0, x0
+    mv s1, x0
+    mv s2, a0
+    mv s3, a1
+    mv s4, a2
+    mv s5, a3 
+    mv s6, a4
+    mv s7, a5
+    mv s8, a6
 outer_loop_start:
-
-
-
-
+    bge s0, a1, outer_loop_end
 inner_loop_start:
-
-
-
-
-
-
-
-
-
+    bge s1, a5, inner_loop_end
+    mul t0, s0, s4
+    slli t0, 2
+    add a0, s2, t0
+    mv t1, s1
+    slli t1, 2
+    add a1, s5, t1
+    call dot
+    
 
 
 
 inner_loop_end:
-
-
 
 
 outer_loop_end:
