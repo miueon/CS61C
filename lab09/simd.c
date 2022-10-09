67649cc9231c11ade0e6292b2513fb0556d0e3e6
miueon <cruxbell@gmail.com>
2022-10-09 15:45:09 +0800

    lab 09

--- lab09/simd.c
+++ lab09/simd.c
@@ -93,7 +93,44 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
   for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
     /* YOUR CODE GOES HERE */
     /* Copy your sum_simd() implementation here, and unroll it */
+    __m128i sum_vecs[4] = {_mm_setzero_si128(), _mm_setzero_si128(), _mm_setzero_si128(), _mm_setzero_si128()};
+    __m128i tmp[4];
+    /* YOUR CODE GOES HERE */
+    for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+      tmp[0] = _mm_loadu_si128((__m128i *)(vals + i));
+      __m128i mask = _mm_cmpgt_epi32(tmp[0], _127);
+      tmp[0] = _mm_and_si128(tmp[0], mask);
+      sum_vecs[0] = _mm_add_epi32(sum_vecs[0], tmp[0]);
 
+      tmp[1] = _mm_loadu_si128((__m128i *)(vals + i + 4));
+      mask = _mm_cmpgt_epi32(tmp[1], _127);
+      tmp[1] = _mm_and_si128(tmp[1], mask);
+      sum_vecs[1] = _mm_add_epi32(sum_vecs[1], tmp[1]);
+
+      tmp[2] = _mm_loadu_si128((__m128i *)(vals + i + 8));
+      mask = _mm_cmpgt_epi32(tmp[2], _127);
+      tmp[2] = _mm_and_si128(tmp[2], mask);
+      sum_vecs[2] = _mm_add_epi32(sum_vecs[2], tmp[2]);
+
+      tmp[3] = _mm_loadu_si128((__m128i *)(vals + i + 12));
+      mask = _mm_cmpgt_epi32(tmp[3], _127);
+      tmp[3] = _mm_and_si128(tmp[3], mask);
+      sum_vecs[3] = _mm_add_epi32(sum_vecs[3], tmp[3]);
+    }
+    int tmp_arr[16];
+    _mm_storeu_si128((__m128i *)tmp_arr, sum_vecs[0]);
+    _mm_storeu_si128((__m128i *) (tmp_arr + 4), sum_vecs[1]);
+    _mm_storeu_si128((__m128i *) (tmp_arr + 8), sum_vecs[2]);
+    _mm_storeu_si128((__m128i *) (tmp_arr + 12), sum_vecs[3]);
+    for (int i = 0; i < 16; i++) {
+        result += tmp_arr[i];
+    }
+
+    for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+      if (vals[i] >= 128) {
+        result += vals[i];
+      }
+    }
     /* Hint: you'll need 1 or maybe 2 tail cases here. */
   }
 
